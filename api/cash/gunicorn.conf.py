aa9af8f000d7583041c65fc4993dcb9a6e6f11cf
anshengme <ianshengme@gmail.com>
2019-01-09 16:06:31 +0800

    fix(api) 修改gunicorn监听的IP地址

--- api/cash/gunicorn.conf.py
+++ api/cash/gunicorn.conf.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TMP_DIR = os.path.join(BASE_DIR, 'tmp')
 
-bind = '0.0.0.0:9006'
+bind = '127.0.0.1:9006'
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = os.path.join(TMP_DIR, 'logs/gunicorn.access.log')
 errorlog = os.path.join(TMP_DIR, 'logs/gunicorn.error.log')
