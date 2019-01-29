1bb04f5a32c5c10a99dc4580e05aee76855017ff
anshengme <ianshengme@gmail.com>
2019-01-30 00:31:08 +0800

    fix(api) gunicorn进程数量设置为CPU数量

--- api/cash/gunicorn.conf.py
+++ api/cash/gunicorn.conf.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TMP_DIR = os.path.join(BASE_DIR, 'tmp')
 
 bind = '127.0.0.1:9006'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count()
 accesslog = os.path.join(TMP_DIR, 'logs/gunicorn.access.log')
 errorlog = os.path.join(TMP_DIR, 'logs/gunicorn.error.log')
 loglevel = 'info'
