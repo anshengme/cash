aa58215612880c247df4569033902d5094659f45
anshengme <ianshengme@gmail.com>
2019-03-05 16:37:48 +0800

    feat(api) 添加rss/atom/sitemap

--- api/cash/urls.py
+++ api/cash/urls.py
@@ -1,8 +1,15 @@
 from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 
+from utils.feeds import RssSiteNewsFeed, AtomSiteNewsFeed
+from utils.sitemap import BlogSitemap
+
 urlpatterns = [
+    path('rss.xml', RssSiteNewsFeed()),
+    path('atom.xml', AtomSiteNewsFeed()),
+    path('sitemap.xml', sitemap, {'sitemaps': {'static': BlogSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
     path('account/', include('cash_account.urls')),
     path('link/', include('cash_link.urls')),
     path('tag/', include('cash_tag.urls')),
