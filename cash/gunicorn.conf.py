522b098137bc666f9f339d6a02a7a49a767221c3
anshengme <ianshengme@gmail.com>
2019-08-12 20:21:17 +0800

    Update gunicorn.conf.py

--- cash/gunicorn.conf.py
+++ cash/gunicorn.conf.py
@@ -1,6 +1,9 @@
+import os
 import multiprocessing
 
-bind = '127.0.0.1:9006'
+BIND_HOST = os.environ.get('BIND_HOST', '127.0.0.1')
+
+bind = f'{BIND_HOST}:9006'
 workers = multiprocessing.cpu_count()
 loglevel = 'info'
 pidfile = '/tmp/gunicorn.pid'
